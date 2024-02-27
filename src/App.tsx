8c75a943a79a425e4a8685cb57f284fecfd00473
Nandan <nandan.karmakar739@gmail.com>
2024-02-27 23:31:25 +0530

    added initial setup files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App: React.FC = function () {
+  return (
+    <Fragment>
+      <h1>Welcome to portfolio</h1>
+    </Fragment>
+  );
+};
+
+export default App;
